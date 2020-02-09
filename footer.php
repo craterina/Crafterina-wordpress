fbca9bc4e8b172bd56f849a395ba582f116a72fd
tianwei <464231692@qq.com>
2020-02-08 23:44:05 -0500

    starter

---
+++ footer.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Craferina
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer">
+		<div>
+			<h2></h2>
+			<p>about</p>
+			<p>contact</p>
+			<p>career</p>
+		</div>
+		<div>
+			<h2>DEAL</h2>
+			<p>Coupons</p>
+			<p>Products to discover</p>
+			<p>Occastional deals</p>
+		</div>
+		<div>
+			<h2>POLICIES</h2>
+			<p>Exchange</p>
+			<p>Terms of use</p>
+			<p>Security</p>
+			<p>Privacy</p>
+		</div>
+		<div>
+			<h2>CONTACT</h2>
+			<p>2000 Simcoe Street, Oshawa, Ontario</p>
+			<p>Tel:2899969530</p>
+			<p>jewellry@craferina.com</p>
+		</div>
+	</footer>
+</div>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Craferina
+ */
+
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<div id="page" class="site">
+<img src="../../uploads/2020/02/crafterina-100x100.png" alt="">
+	<header id="masthead" class="site-header">
+		<img src="../../uploads/2020/02/crafterina-100x100.png" alt="">
+
+	</header>
+	<img src="../../uploads/2020/02/crafterina-100x100.png" alt="">
+<div id="content" class="site-content">
